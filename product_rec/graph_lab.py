86808894d445633124d5fe76ecb7be272a6a6894
Shai Wilson <sjwilson2@usfca.edu>
2016-02-09 15:27:32 -0800

    add graph_lab model.

---
+++ product_rec/graph_lab.py
@@ -0,0 +1,107 @@
+# Author : Shai Wilson
+""" Read seed_data and train images """
+
+import graphlab as gl
+gl.canvas.set_target('browser', 5000)
+
+# .show on a graphlab object returns a json object 
+#
+# [column_names]
+# 0000=id
+# 0001=image
+# 0002=label
+# 0003=deep_features
+# 0004=image_array
+
+# ##############################################################################
+# # Part 1: Read Data
+
+def read_images():
+
+    """Loads data set into SFRAMES"""
+
+    print "*********************"
+    print "Reading the data"
+
+    image_train=gl.SFrame('seed_data/image_train_data/')
+    image_test=gl.SFrame('seed_data/image_test_data/')
+
+def _tests():
+    print "*********************"
+    # print image_train['id']
+    print "*********************"
+
+    print "*********************"
+    print "expecting 4000 images:", len(image_test)
+    print "*********************"
+
+def train_images():
+	""" IMAGE CLASSIFICATION TASK
+		Train a classifier on the raw image pixels using transfered learning
+		deep_feautures already contains the pre-computed deep features for this data """
+
+    print "*********************"
+    print "training the model"
+    print image_train.head()
+    print "*********************"
+
+
+# features = deep_features pretrained
+# target = thing i'm trying to predict is given by the label column
+# creating a classifier on 4000 images using features computed in 
+# the neural network from: http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45
+# deep_features_model = gl.logistic_classifier.create(image_train,
+#                                                          features=['deep_features'],
+#                                                          target='label')
+
+def image_retrieval():
+	""" IMAGE RETREVIAL TASK
+	    Create a nearest neighbors model for image retrieval 
+	    train nearest neighbors model for retrieving images using deep features"""
+
+    knn_model = gl.nearest_neighbors.create(image_train,features=['deep_features'],
+                                                 						label='id')
+
+
+# # apply the deep features model to the images that the user chooses
+# print "*********************"
+
+# TODO
+# covert the column tag_category to a dictionary
+
+
+
+# ##############################################################################
+# # Helper functions
+
+# Overview:
+# server.py obtains image id from db, user clicks
+# sends it here to find the nearest neighbors
+
+def get_images_from_ids(query_result):
+	"""	Use image retrieval model with deep features to find similar images
+	expecting : pair_of_images = image_train[image_id_1:image_id_2]
+	neighbors = get_images_from_ids(knn.model.query(pair_of_images))
+	neighbors['image'].show """
+
+	print "*********************"
+	print "Reading in images and making predictions"
+	print "*********************"
+
+    return image_train.filter_by(query_result['reference_label'],'id')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- product_rec/model.py
+++ product_rec/model.py
@@ -4,69 +4,6 @@
 # from flask_sqlalchemy import SQLAlchemy
 # db = SQLAlchemy()
 
-import graphlab as gl
-
-# .show on a graphlab object returns a json object 
-
-# load data set into SFRAMES
-
-print "*********************"
-print "Reading the data"
-image_train=gl.SFrame('seed_data/image_train_data/')
-image_test=gl.SFrame('seed_data/image_test_data/')
-gl.canvas.set_target('browser', 5000)
-
-print "*********************"
-print "expecting 2005 images:", len(image_test)
-print "*********************"
-
-print "*********************"
-print "training the model"
-print image_train.head()
-print "*********************"
-
-# IMAGE CLASSIFICATION TASK
-# Train a classifier on the raw image pixels using transfered learning
-# deep_feautures already contains the pre-computed deep features for this data
-# features = deep_features pretrained
-# target = thing i'm trying to predict is given by the label column
-# creating a classifier on 2000 images using features computed in 
-# the neural network from: http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45
-# deep_features_model = gl.logistic_classifier.create(image_train,
-#                                                          features=['deep_features'],
-#                                                          target='label')
-
-# IMAGE RETREVIAL TASK
-# Create a nearest neighbors model for image retrieval 
-# train nearest neighbors model for retrieving images using deep features
-knn_model = graphlab.nearest_neighbors.create(image_train,features=['deep_features'],
-                                             label='id')
-
-
-
-print "Reading image_test and making predictions"
-# apply the deep features model to the images that the user chooses
-print "*********************"
-
-# TODO
-# covert the column tag_category to a dictionary
-
-
-# ##############################################################################
-# # Part 1: Models
-
-
-
-# ##############################################################################
-# # Helper functions
-
-# Use image retrieval model with deep features to find similar images
-# expecting : pair_of_images = image_train[image_id_1:image_id_2]
-# neighbors = get_images_from_ids(knn.model.query(pair_of_images))
-# neighbors['image'].show
-def get_images_from_ids(query_result):
-    return image_train.filter_by(query_result['reference_label'],'id')
-
 def init_app():
     # So that we can use Flask-SQLAlchemy, we'll make a Flask app
     from flask import Flask
@@ -80,7 +17,7 @@ def init_app():
 #     """Connect the database to our Flask app."""
 
 #     # Configure to use our SQLite database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cars'
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///images'
 #     app.config['SQLALCHEMY_ECHO'] = True
 #     db.app = app
 #     db.init_app(app)
