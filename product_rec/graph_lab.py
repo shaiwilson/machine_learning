592202fb9b6d23bf5abd99710a8b93431ad3ed60
Shai Wilson <sjwilson2@usfca.edu>
2016-02-11 13:48:14 -0800

    Organize

--- product_rec/graph_lab.py
+++ product_rec/graph_lab.py
@@ -14,62 +14,58 @@ import graphlab as gl
 gl.canvas.set_target('browser')
 
 
+
 # ##############################################################################
-# # Part 1: Read Data
+# # Part 1: Save Data to CSV
+
+def read_data(image_test):
+
+	"""Save image_train data to csv"""
+
+	# inspect the images in the data set
+	print "*********************"
+	print "expecting 4000 images:", len(image_test)
+	print "*********************"
 
 
-"""Loads data set into SFRAMES
+	# [column_names]
+	# 0000=id
+	# 0001=image
+	# 0002=label
+	# 0003=deep_features
+	# 0004=image_array
 
-	IMAGE CLASSIFICATION TASK
-	Train a classifier on the raw image pixels using transfered learning
-	deep_feautures already contains the pre-computed deep features for this data. """
+	print "*********************"
+	print "save image labels to csv"
+	all_image_labels = image_test['label']
+	image_labels = gl.SArray(all_image_labels)
+	all_image_labels.save('all_image_labels.csv')
+	print "*********************"
+	all_image_ids = image_test['id']
+	image_id = gl.SArray(all_image_ids)
+	print "*********************"
+	print "create a csv file concatenated with the id and label"
+	image_id_and_label = gl.SFrame({'ids': image_id, 'label': image_labels})
+	image_id_and_label.save('image_id_and_label.csv')
+	print "*********************"
 
-print "*********************"
-print "Reading the data"
+# ##############################################################################
+# # Part 2: Use a pre-trained Neural Network to train the images
+def train_images(image_train):
 
-image_train=gl.SFrame('seed_data/image_train_data/')
-image_test=gl.SFrame('seed_data/image_test_data/')
+		""" IMAGE CLASSIFICATION TASK
+		Train a classifier on the raw image pixels using transfered learning
+		deep_feautures already contains the pre-computed deep features for this data. """
 
-
-# inspect the images in the data set
-print "*********************"
-print "expecting 4000 images:", len(image_test)
-print "*********************"
-
-print "*********************"
-print "training the model"
-print image_train.head()
-print "*********************"
-
-# [column_names]
-# 0000=id
-# 0001=image
-# 0002=label
-# 0003=deep_features
-# 0004=image_array
-
-print "*********************"
-print "save image labels to csv"
-all_image_labels = image_test['label']
-image_labels = gl.SArray(all_image_labels)
-all_image_labels.save('all_image_labels.csv')
-print "*********************"
-all_image_ids = image_test['id']
-image_id = gl.SArray(all_image_ids)
-print "*********************"
-print "create a csv file concatenated with the id and label"
-image_id_and_label = gl.SFrame({'ids': image_id, 'label': image_labels})
-image_id_and_label.save('image_id_and_label.csv')
-print "*********************"
-
-
-# features = deep_features pretrained
-# target = thing i'm trying to predict is given by the label column
-# creating a classifier on 4000 images using features computed in 
-# the neural network from: http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45
-# deep_features_model = gl.logistic_classifier.create(image_train,
-#                                                          features=['deep_features'],
-#                                                          target='label')
+		# features = deep_features pretrained
+		# target = thing i'm trying to predict is given by the label column
+		# creating a classifier on 4000 images using features computed in 
+		# the neural network from: http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45
+		
+		deep_features_model = gl.logistic_classifier.create(image_train,
+		                                                         features=['deep_features'],
+		                                                         target='label')
+		return deep_features_model
 
 
 
@@ -104,7 +100,21 @@ apply the deep features model to the images that the user chooses.
 
 #     return image_train.filter_by(query_result['reference_label'],'id')
 
+if __name__ == "__main__":
 
+	print "*********************"
+	print "Reading the data"
+
+	image_train=gl.SFrame('seed_data/image_train_data/')
+	image_test=gl.SFrame('seed_data/image_test_data/')
+
+    print "*********************"
+	print "training the model"
+	# train_images(image_train)
+	print "*********************"
+
+    # load_ratings()
+    # set_val_user_id()
 
 
 
