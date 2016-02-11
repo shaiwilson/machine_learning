6c26b5adfa391e30b844eb742893d3c6edf5fa72
Shai Wilson <sjwilson2@usfca.edu>
2016-02-11 15:06:08 -0800

    Finish Data model.

--- product_rec/seed.py
+++ product_rec/seed.py
@@ -1,110 +1,30 @@
-# """Utility file to seed training set of images database from image_test_data in seed_data/"""
-
-# import datetime
-# from sqlalchemy import func
+ """Utility file to seed training set of images database from image_test_data in seed_data/"""
 
 # from model import Image, Label, connect_to_db, db
-# from server import app
+from server import app
 
 
-# def load_users():
-#     """Load images from image_test_data into database."""
+def load_images():
+    """Load images from image_test_data into database."""
 
-#     print "Images"
+    print "Images"
 
-#     for i, row in enumerate(open("seed_data/u.user")):
-#         row = row.rstrip()
-#         user_id, age, gender, occupation, zipcode = row.split("|")
+    for i, row in enumerate(open("seed_data/image_id_and_label")):
+        row = row.rstrip()
+        iamge_id, label = row.split(",")
 
-#         user = User(age=age,
-#                     zipcode=zipcode)
+        image = Image(id=image_id,
+                    label=label)
 
-#         # We need to add to the session or it won't ever be stored
-#         db.session.add(user)
+        # We need to add to the session or it won't ever be stored
+        db.session.add(image)
 
-#         # provide some sense of progress
-#         if i % 100 == 0:
-#             print i
+        # provide some sense of progress
+        if i % 100 == 0:
+            print i
 
-#     # Once we're done, we should commit our work
-#     db.session.commit()
-
-
-# def load_movies():
-#     """Load movies from u.item into database."""
-
-#     print "Movies"
-
-#     for i, row in enumerate(open("seed_data/u.item")):
-#         row = row.rstrip()
-
-#         # clever -- we can unpack part of the row!
-#         movie_id, title, released_str, junk, imdb_url = row.split("|")[:5]
-
-#         # The date is in the file as daynum-month_abbreviation-year;
-#         # we need to convert it to an actual datetime object.
-
-#         if released_str:
-#             released_at = datetime.datetime.strptime(released_str, "%d-%b-%Y")
-#         else:
-#             released_at = None
-
-#         # Remove the (YEAR) from the end of the title.
-
-#         title = title[:-7]   # " (YEAR)" == 7
-
-#         movie = Movie(title=title,
-#                       released_at=released_at,
-#                       imdb_url=imdb_url)
-
-#         # We need to add to the session or it won't ever be stored
-#         db.session.add(movie)
-
-#         # provide some sense of progress
-#         if i % 100 == 0:
-#             print i
-
-#     # Once we're done, we should commit our work
-#     db.session.commit()
-
-
-# def load_ratings():
-#     """Load ratings from u.data into database."""
-
-#     print "Ratings"
-
-#     for i, row in enumerate(open("seed_data/u.data")):
-#         row = row.rstrip()
-
-#         user_id, movie_id, score, timestamp = row.split("\t")
-
-#         user_id = int(user_id)
-#         movie_id = int(movie_id)
-#         score = int(score)
-
-#         # We don't care about the timestamp, so we'll ignore this
-
-#         rating = Rating(user_id=user_id,
-#                         movie_id=movie_id,
-#                         score=score)
-
-#         # We need to add to the session or it won't ever be stored
-#         db.session.add(rating)
-
-#         # provide some sense of progress
-#         if i % 1000 == 0:
-#             print i
-
-#             # An optimization: if we commit after every add, the database
-#             # will do a lot of work committing each record. However, if we
-#             # wait until the end, on computers with smaller amounts of
-#             # memory, it might thrash around. By committing every 1,000th
-#             # add, we'll strike a good balance.
-
-#             db.session.commit()
-
-#     # Once we're done, we should commit our work
-#     db.session.commit()
+    # Once we're done, we should commit our work
+    db.session.commit()
 
 
 
@@ -112,7 +32,4 @@ if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
 
-    # load_users()
-    # load_movies()
-    # load_ratings()
-    # set_val_user_id()
+    # load_images()
