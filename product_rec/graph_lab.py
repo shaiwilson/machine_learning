268b4dc40d3ad7b67c6f177830e8ff23d279b48e
Hackbright Student <info@hackbrightacademy.com>
2016-02-12 15:06:59 -0800

    fix merge.

--- product_rec/graph_lab.py
+++ product_rec/graph_lab.py
@@ -30,70 +30,9 @@ print "Reading the data"
 image_train=gl.SFrame('seed_data/image_train_data/')
 image_test=gl.SFrame('seed_data/image_test_data/')
 
-# save data table as a text file
-image_test.save('my_image_data', format='csv')
-
-# inspect the images in the data set
-print "*********************"
-print "expecting 4000 images:", len(image_test)
-print "*********************"
-
-print "*********************"
-print "training the model"
-print image_train.head()
-print "*********************"
-
 # the data set contains some repeated labels, for instance, cat, dogs
 # use the deduplication toolkit to remove copies
 # how it works: injests data from SFrames and assigns and entity label to each row
-
-# ERROR UNABLE TO AUTOMATICFALLY CONSTRUCT A DISTANCE FUNCTIONFOR FEATURE 'deep_features'
-
-"""
-https://dato.com/learn/userguide/data_matching/deduplication.html
-https://dato.com/learn/gallery/notebooks/machine_learning_with_graphLab_create.html
-https://dato.com/learn/gallery/notebooks/food_retrieval-public.html
-https://dato.com/learn/gallery/notebooks/deep_text_learning.html
-https://dato.com/learn/gallery/
-"""
-dedup = gl.nearest_neighbor_deduplication.create(image_test, row_label='label',
-                                                             grouping_features=['label'],
-                                                             radius=0.25)
-
-print "*********************"
-print "output entity models"
-print dedup.summary()
-print "*********************"
-
-# model's entities attribute contains the deduplication results
-# select one element from each entity group to be in my clean label dataset
-# add label frequency (count number of images per group)
-entities = dedup['entities']
-
-labels_clean = entities.groupby('__entity', operations = {'row_number' : gl.aggregate.SELECT_ONE('row_number')})
-
-# find the dupe labels
-label_count = dedup['entities'].groupby('__entity', gl.aggregate.COUNT)
-dupe_entities = label_count[label_count['Count'] > 1]['__entity']
-
-# print the results for the dupes
-dupes = labels_clean.filter_by(dupe_entities, '__entity')
-dupes.print_rows(10, max_row_width=100, max_column_width=50)
-
-# clean my data
-# image_test_clean = image_test.add_row_number('row_number').filter_by(dedup_test_labels['row_number'], 'row_number')
-# image_test_clean['label']
-
-# features = deep_features pretrained
-# target = thing i'm trying to predict is given by the label column
-# creating a classifier on 4000 images using features computed in 
-# the neural network from: http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45
-# deep_features_model = gl.logistic_classifier.create(image_train,
-#                                                          features=['deep_features'],
-#                                                          target='label')
-
-
-# # Part 1: Save Data to CSV
 
 def read_data(image_test):
 
