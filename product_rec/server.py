0b8860dd4ea1117fa1eee820831409d2e33c395d
Hackbright Student <info@hackbrightacademy.com>
2016-02-12 17:29:27 -0800

    TODO fix html.

--- product_rec/server.py
+++ product_rec/server.py
@@ -5,7 +5,7 @@
 from jinja2 import StrictUndefined
 
 # from flask_debugtoolbar import DebugToolbarExtension
-import visuals
+
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 
 app = Flask(__name__)
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
     app.debug = True
-    visuals.connect_to_db(app)
+    # visuals.connect_to_db(app)
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
