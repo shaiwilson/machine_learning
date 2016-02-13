9eb516e2946f916351d1f309647e44bfc992a47b
Hackbright Student <info@hackbrightacademy.com>
2016-02-12 17:23:05 -0800

    Add favorites route.

--- product_rec/server.py
+++ product_rec/server.py
@@ -5,7 +5,7 @@
 from jinja2 import StrictUndefined
 
 # from flask_debugtoolbar import DebugToolbarExtension
-
+import visuals
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 
 app = Flask(__name__)
@@ -31,11 +31,20 @@ def image_list():
     # images = Image.query.all()
     # return render_template("image_list.html", images=image)
 
+# @app.route("/add-to-favorites", methods=["POST"])
+# def add_to_favorites():
+
+#     photo_id = request.form.get("id")
+
+#     # put this in a "favorites" table?
+
+#     return jsonify(status="success", id=photo_id)
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
     app.debug = True
-    connect_to_db(app)
+    visuals.connect_to_db(app)
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
