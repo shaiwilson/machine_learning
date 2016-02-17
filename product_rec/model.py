a364c954791f94b276a02842e04306d75e16e341
Hackbright Student <info@hackbrightacademy.com>
2016-02-17 09:08:47 -0800

    add image tag model.

--- product_rec/model.py
+++ product_rec/model.py
@@ -14,16 +14,40 @@ class Image(db.Model):
     __tablename__ = "images"
 
     image_id = db.Column(db.Integer, primary_key=True)
-    image_label = db.Column(db.String(64), nullable=False)
-
-    # EXTRA FEATURES
-    # this needs to be list of tags added by the user
-    # image_tags = db.Column(db.String(64), nullable=False)
+    image_label = db.Column(db.String(64), nullable=False) 
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
         return "<Image image_id=%s label=%s>" % (self.image_id, self.image_label)
+
+class Tag(db.Model):
+    """Association table for image tags."""
+
+    __tablename__ = "tags"
+
+    tag_id = db.Column(db.Integer, primary_key=True)
+    tag_label = db.Column(db.String(64), nullable=False)
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Tag tag_id=%s label=%s>" % (self.tag_id, self.tag_label)
+
+class Image_Tags(db.Model):
+    """Model many-to-Many relationship between images and tags."""
+
+    __tablename__ = "imagetags"
+
+    image_id = db.Column(db.Integer, primary_key=True)
+    tag_id = db.Column(db.Integer, nullable=False)
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Tag image_id=%s tag_id=%s>" % (self.iamge_id, self.tag_id)
 
 
 def init_app():
