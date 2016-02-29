75e99829b78f7c14dda38f20cdd50a98ddd65a9d
Shai Wilson <sjwilson2@usfca.edu>
2016-02-29 15:26:52 -0800

    run tests

--- product_rec/server.py
+++ product_rec/server.py
@@ -23,7 +23,6 @@ app.secret_key = "ABC"
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
-
 @app.route('/welcome')
 def train():
     """Train image data."""
@@ -36,35 +35,65 @@ def train():
     knn_model = graph_lab.knn_model_generic(image_train)
     cat = image_train[18:19]
     img_neighbors = graph_lab.get_images_from_ids(image_train, knn_model.query(cat))
-    image_list(img_neighbors)
-    
   
+    images = [[] for i in range(len(img_neighbors))]
+    np_image_array = img_neighbors['image_array'].to_numpy()
+    print type(np_image_array)
+    print np_image_array.shape
+    
+
+   
+    # for i, v in enumerate(np_image_array):
+    #     # print "i is: " , i
+    #     # print "v is: " , v
+    #     print v 
+    #     img_reshaped = np.reshape(v, (32,32,3))
+
+
+    #     img = Image.fromarray(img_reshaped, 'RGB')
+    #     img_index = str(i)
+    #     img_name = "img_" + img_index 
+    #     img.save('static/imgs/' + img_name + ".PNG")
+    #     image_path = 'static/imgs/' + img_name + ".PNG" 
+    #     images[i] = image_path
+  
+
     return render_template("welcome.html")
 
-@app.route("/images")
-def image_list(img_neighbors):
+# button on the welcome page that redirects to the show page
+@app.route('/show')
+def get_images(img_neighbors):
+    """ Call show image function """
+
+    images = show_list(img_neighbors)
+    
+    return render_template("imagelist.html", images=images)
+
+def show_list(img_neighbors):
     """Show grid images."""
 
     # img_neighbors is a numpy array of images
     # images is an empty list of dictionary images
-    images = [{} for i in range(len(img_neighbors))]
+    images = [[] for i in range(len(img_neighbors))]
     np_image_array = img_neighbors['image_array'].to_numpy()
+    print img_neighbors['image_array']
 
+    format = 'PNG'
     for i, v in enumerate(np_image_array):
         # print "i is: " , i
         # print "v is: " , v
-
-        # img1 = np_image_array[i]
+        print v 
         img_reshaped = np.reshape(v, (32,32,3))
-        img = Image.fromarray(img_reshaped, 'RGBA')
-        img_name = 'img', i, '.png'
-        img.save(img_name)
-        # img_deats = {'id': i, 'url' : img_name}
-        # images.append(img_deats)
-   
-    return images
+
+
+        img = Image.fromarray(img_reshaped, 'RGB')
+        img_index = str(i)
+        img_name = "img_" + img_index 
+        img.save('static/imgs/' + img_name + ".PNG")
+        image_path = 'static/imgs/' + img_name + ".PNG" 
+        images[i] = image_path
   
-    # return render_template("imagelist.html", images=images)
+    return images 
 
 @app.route('/cats')
 def show_cats(image_train):
@@ -74,9 +103,12 @@ def show_cats(image_train):
     knn_model = graph_lab.knn_model_generic(image_train)
     cat_neighbors = graph_lab.get_images_from_ids(image_train, knn_model.query(cat))
     print cat_neighbors
-    # cat_neighbors['image'].show()
-    # image_list(cats)
 
+
+@app.route('/show')
+def view_method():
+    response = send_file(tempFileObj, as_attachment=True, attachment_filename='myfile.jpg')
+    return response
 
 @app.route('/')
 def index():
